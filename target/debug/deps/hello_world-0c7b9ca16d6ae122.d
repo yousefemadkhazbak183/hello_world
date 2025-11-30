5142b2e2058fcfa7caaeab9bed99554502668d2d
Yousef Emad <yousefemadkhazbak@gmail.com>
2025-11-30 21:43:32 +0200

    - Init First Project int Rust.

---
+++ target/debug/deps/hello_world-0c7b9ca16d6ae122.d
@@ -0,0 +1,5 @@
+/Users/karimebrahim/Downloads/hello_world/target/debug/deps/hello_world-0c7b9ca16d6ae122.d: src/main.rs
+
+/Users/karimebrahim/Downloads/hello_world/target/debug/deps/libhello_world-0c7b9ca16d6ae122.rmeta: src/main.rs
+
+src/main.rs:
